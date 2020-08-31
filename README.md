01d08a3084ab2165095c813db1a7ae99d77c3a1d
Max <maxance.lloy@gmail.com>
2020-08-31 17:31:27 +0200

    finish

--- README.md
+++ README.md
@@ -6,6 +6,8 @@
 2. The form file input field  has the "name" attribute set to "upfile". We rely on this in testing.
 3. When I submit something, I will receive the file name and size in bytes within the JSON response
 
+See : {"name":"macos-catalina-5120x2880-night-mountains-wwdc-2019-5k-21589.jpg","type":"image/jpeg","size":3181550}
+
 ### Usage :
 * Go to the main page, and upload a file using the provided form.
 
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 'use strict';
 
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer  = require('multer')
+const upload = multer({ dest: './' })
 
-// require and use "multer"...
-
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -13,6 +13,11 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res)=>{
+    let {originalname:name, mimetype:type, size} = req.file;
+    res.json({name, type, size});
+});
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
